de6486e52888e73eb26d0dcf93a6808f98460d28
Vladimír Pinďura <vladimir.pindura@viareality.cz>
2016-10-20 13:02:25 +0200

    pridani jquery

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+var $ = require('jquery');
 
 @Component({
   selector: 'app-root',
@@ -17,7 +18,36 @@ export class AppComponent {
   constructor() {
     this.loadingInProgress = false; 'kliknuti na loadMore'
     this.switchLoading();
+
+    console.log($);
   }
+
+//http://stackoverflow.com/a/33226843
+//   function listenScroll()
+//   {
+//     var scrolling = Rx.Observable.
+//         fromEvent($('.infinite')[0], 'scroll').
+//         map((se)=> {
+//             return {
+//                 scrollTop:se.target.scrollTop,
+//                 scrollHeight: se.target.scrollHeight,
+//                 clientHeight: se.target.clientHeight
+//             };
+//         }).
+//         filter((x)=> x.clientHeight ===  x.scrollHeight - x.scrollTop);
+
+// mouseup$ = Rx.Observable.fromEvent($('.infinite')[0], 'mouseup');
+// // mouseup or keyup or touchend or else
+// var getNewData$ = scrolling
+//     .flatMap(function (scroll_data) {
+//         return Rx.Observable
+//             .just(scroll_data)
+//             .delay(1000)
+//             .takeUntil(mouseup$);
+//     });
+//   }
+
+
 
   loadData() {
     var self = this;
